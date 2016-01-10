bd9f70127491d57d8b3630e84cd7b0412444ed91
msturrus <msturrus@gmail.com>
2016-01-10 12:20:16 -0500

    This Week's Assignment
    Hi Matt,
    
    Sorry this assignment is late but I commit from the github app itself
    and it didn’t take e for some reason yesterday and I just noticed it
    today.
    
    My slide toggles do not work!  I looked everywhere for what I was doing
    wrong and could not find it.

--- 08_Intro_jQuery/starter_code/jquery-examples/js/main.js
+++ 08_Intro_jQuery/starter_code/jquery-examples/js/main.js
@@ -1,8 +1,9 @@
+
 // --- FINDING ELEMENTS ---
 // travers the DOM and find/select elements to work with
 
 // .find()
-
+$('ol').find('.special');
 // .children()
 
 // .parent()
@@ -14,9 +15,9 @@
 // Get/change content of elements, attributes, and text nodes
 
 // .html()
-
+$('h1').html('Yeah, I knew that!');
 // .attr()
-
+$('img').attr('src', 'images/jackie.jpg');
 // .css()
 
 // .addClass()
@@ -30,9 +31,9 @@
 // Add effects and animation to parts of the page
 
 // .show()
-
+//$('h1').show();
 // .hide()
-
+//$('h1').hide();
 // .fadeIn()
 
 // .fadeOut()
@@ -48,7 +49,9 @@
 // Select elements and have them 'listen' for certain event to trigger changes/functions
 
 // .on('click')
-
+$('h1').on('click', function() {
+	$('h1').slideToggle(1000);
+});
 
 // Others: 
 // .on('focus')
